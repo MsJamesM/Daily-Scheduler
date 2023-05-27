c30c3dfde05be148095c2c611b924aea9aa4bd62
MsJamesM <118479305+MsJamesM@users.noreply.github.com>
2023-05-26 21:32:57 -0600

    implemented some local storage in JS file. Tweaked HTML file

--- script.js
+++ script.js
@@ -2,5 +2,25 @@
 
 var clock = document.getElementById("#clock");
 var today = dayjs();
-console.log(today);
+
 $("#clock").text(today.format("MMM D, YYYY"));
+
+// ---------------------------------------------------------- local storage
+
+localStorage.getItem("hour9");
+localStorage.getItem("hour10");
+localStorage.getItem("hour11");
+localStorage.getItem("hour12");
+localStorage.getItem("hour13");
+localStorage.getItem("hour14");
+localStorage.getItem("hour15");
+localStorage.getItem("hour16");
+localStorage.getItem("hour17");
+localStorage.getItem("hour18");
+
+$(".saveBtn").click(function () {
+  var text = $(this).siblings("textArea").val();
+  var time = $(this).parent().attr("id");
+
+  localStorage.setItem(time, text);
+});
