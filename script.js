727f0c27e10dcfce25e492ba0676347150108020
MsJamesM <118479305+MsJamesM@users.noreply.github.com>
2023-05-26 22:33:00 -0600

    fixed pesky localstorage issues

--- script.js
+++ script.js
@@ -7,22 +7,28 @@ $("#clock").text(today.format("MMM D, YYYY"));
 
 // ---------------------------------------------------------- local storage
 
-localStorage.getItem("hour9");
-localStorage.getItem("hour10");
-localStorage.getItem("hour11");
-localStorage.getItem("hour12");
-localStorage.getItem("hour13");
-localStorage.getItem("hour14");
-localStorage.getItem("hour15");
-localStorage.getItem("hour16");
-localStorage.getItem("hour17");
-localStorage.getItem("hour18");
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
+$("#hour18 .description").val(localStorage.getItem("hour18"));
 
 $(".saveBtn").click(function () {
   var text = $(this).siblings("textArea").val();
   var time = $(this).parent().attr("id");
 
   localStorage.setItem(time, text);
+});
+
+$("#clearFieldsBtn").click(function (event) {
+  event.preventDefault;
+  $("textarea").val("");
+  localStorage.clear();
 });
 
 // ---------------------------------------- textarea past, present, future
